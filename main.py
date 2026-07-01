a35cca1f18c2cf209f3cb8f30e2c6b626e402a99
maturuuu <matthew_kristoffer_ong@dlsu.edu.ph>
2026-07-01 13:24:46 +0800

    feat: added server handoff from VAD to ASR

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
+import os
 import io
 import wave
+import httpx
 from fastapi import FastAPI, Depends, HTTPException, File, UploadFile, Query
 from fastapi.responses import Response
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import text
 from database import get_db
 from vad_utils import apply_vad_filter
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI(title="HeyRoute API")
+
+ASR_URL = "http://altdsidccf.dlsu.edu.ph:33070/transcribe"
+ASR_API_KEY = os.getenv("ASR_API_KEY")
+if not ASR_API_KEY:
+	raise ValueError("CRITICAL ERROR: ASR_API_KEY is missing from the environment variables.")
+
 
 @app.get("/health")
 async def health_check():
@@ -31,26 +42,45 @@ async def process_voice_activity(
 
 		clean_audio_bytes = apply_vad_filter(audio_bytes)
 
+		wav_io = io.BytesIO()
+		with wave.open(wav_io, 'wb') as wav_file:
+			wav_file.setnchannels(1)  # Mono
+			wav_file.setsampwidth(2)  # 16-bit samples
+			wav_file.setframerate(16000)  # 16 kHz sample rate
+			wav_file.writeframes(clean_audio_bytes)
+
+		wav_io.seek(0)
+
 		if download:
-			wav_io = io.BytesIO()
-			with wave.open(wav_io, 'wb') as wav_file:
-				wav_file.setnchannels(1)  # Mono
-				wav_file.setsampwidth(2)  # 16-bit samples
-				wav_file.setframerate(16000)  # 16 kHz sample rate
-				wav_file.writeframes(clean_audio_bytes)
-			
 			return Response(
 				content = wav_io.getvalue(),
 				media_type = "audio/wav",
 				headers = {"Content-Disposition": f"attachment; filename=clean_{audio_file.filename}.wav"}
 			)
 		
+		async with httpx.AsyncClient(timeout=30.0) as client:
+			files = {'file': (f"clean_{audio_file.filename}.wav", wav_io, "audio/wav")}
+			headers = {'X-API-Key': ASR_API_KEY}
+
+			asr_response = await client.post(ASR_URL, files=files, headers=headers)
+
+			if asr_response.status_code != 200:
+				raise HTTPException(status_code = asr_response.status_code,
+									detail = f"ASR Server Error: {asr_response.text}"
+				)
+			
+			transcription_data = asr_response.json()
+		
+		# return to the user
 		return {
 			"filename": audio_file.filename,
 			"original_bytes": len(audio_bytes),
 			"clean_bytes": len(clean_audio_bytes),
-			"message": "Audio processed through VAD filter successfully."
+			"transcription": transcription_data,
+			"message": "Audio processed and transcribed successfully."
 		}
 	
+	except httpx.RequestError as exc:
+		raise HTTPException(status_code=500, detail=f"Failed to connect to ASR server: {str(exc)}")
 	except Exception as e:
 		raise HTTPException(status_code=500, detail=f"Error processing audio file: {str(e)}")
