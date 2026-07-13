a2b6b5e179feb9e1c75c4e387e40538022475dce
maturuuu <matthew_kristoffer_ong@dlsu.edu.ph>
2026-07-13 16:38:03 +0800

    chore: disabled db engine echo

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import declarative_base
 
 SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://heyroute_app:heyroute-db-123@localhost/heyroute"
-engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=False) # Set echo=True if you want to make the db engine verbose
 SessionLocal = async_sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
 
